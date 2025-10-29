04b38f8517f140b975b6cdbb8b7f65213c7043b7
abdullateef <atandaabdullateef90@gmail.com>
2025-10-29 19:27:25 +0100

    changes in the database

--- db.py
+++ db.py
@@ -1,29 +1,23 @@
 # db.py
+import psycopg2
+from psycopg2 import pool
 import os
-import mysql.connector
-from mysql.connector import pooling
-from dotenv import load_dotenv
-
-load_dotenv()
 
 DB_POOL = None
 
 def init_db_pool():
     global DB_POOL
-    if DB_POOL is None:
-        DB_POOL = pooling.MySQLConnectionPool(
-            pool_name="mypool",
-            pool_size=int(os.getenv("DB_POOL_SIZE", "5")),
-            host=os.getenv("DB_HOST", "127.0.0.1"),
-            port=int(os.getenv("DB_PORT", "21012")),
-            user=os.getenv("DB_USER", "root"),
-            password=os.getenv("DB_PASSWORD", ""),
-            database=os.getenv("DB_NAME", "country_cache"),
-            charset="utf8mb4",
-            use_unicode=True
-        )
+    DB_POOL = pool.SimpleConnectionPool(
+        1, 10,
+        host=os.getenv("DB_HOST"),
+        port=os.getenv("DB_PORT"),
+        database=os.getenv("DB_NAME"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD")
+    )
 
-def get_conn():
-    if DB_POOL is None:
-        init_db_pool()
-    return DB_POOL.get_connection()
+def get_db_connection():
+    return DB_POOL.getconn()
+
+def release_db_connection(conn):
+    DB_POOL.putconn(conn)
